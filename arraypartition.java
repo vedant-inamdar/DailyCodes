9d6f915432c46c45b6375fe4f35677a9f38fdd12
vedant-inamdar <vsinamdar8787@gmail.com>
2024-03-29 15:13:43 +0530

    done

---
+++ arraypartition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class arraypartition {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum = sum + nums[i];
+        }
+        return sum;
+    }
+}
