c4575df65be0d8474414341661f893920aa08870
vedant-inamdar <vsinamdar8787@gmail.com>
2024-04-03 22:59:03 +0530

    done

---
+++ uniquenumofoccurences.java
@@ -0,0 +1,18 @@
+class uniquenumofoccurences {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> occurrencesMap = new HashMap<>();
+        for (int i : arr) {
+            occurrencesMap.put(i, occurrencesMap.getOrDefault(i, 0) + 1);
+        }
+        
+        HashSet<Integer> uniqueCountsSet = new HashSet<>();
+
+        for (int count : occurrencesMap.values()) {
+            if (!uniqueCountsSet.add(count)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
