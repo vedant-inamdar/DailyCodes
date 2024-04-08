564be038b7e9d613d1177bc9a1d29c2777fc5def
vedant-inamdar <vsinamdar8787@gmail.com>
2024-04-08 16:25:32 +0530

    done

---
+++ thirdmaxnum.java
@@ -0,0 +1,15 @@
+class thirdmaxnum {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        int uniquecount=1   ;
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i]!=nums[i-1]){
+                uniquecount++;
+                if(uniquecount==3){
+                    return nums[i-1];
+                }
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
