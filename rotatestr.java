0453e31f1ab0f79fe78dba0d203a8a188b5e7eba
vedant-inamdar <vsinamdar8787@gmail.com>
2024-04-10 00:12:40 +0530

    done

--- rotatestr.java
+++ rotatestr.java
@@ -1,14 +1,9 @@
 public class rotatestr {
     public boolean rotateString(String s, String goal) {
-        // If lengths of s and goal are different, they cannot be rotations of each other
         if (s.length() != goal.length()) {
             return false;
         }
-        
-        // Concatenate s with itself to handle rotation
-        String rotated = s + s;
-        
-        // Check if goal is a substring of the concatenated string
+        String rotated = s + s;        
         return rotated.contains(goal);
     }
 }
