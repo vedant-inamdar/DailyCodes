ccbd0404d5906f874c9d768b8df6118b1d726393
vedant-inamdar <vsinamdar8787@gmail.com>
2024-04-14 12:31:10 +0530

    done

--- sumofleftleaves.java
+++ sumofleftleaves.java
@@ -1,4 +1,23 @@
 public class sumofleftleaves {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public int sumOfLeftLeaves(TreeNode root) {
         if (root == null)
             return 0;
