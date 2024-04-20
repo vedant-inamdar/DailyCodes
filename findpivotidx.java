9da998349dbd33128d2ece5efc52f892df9f021b
vedant-inamdar <vsinamdar8787@gmail.com>
2024-04-20 23:37:03 +0530

    done

---
+++ findpivotidx.java
@@ -0,0 +1,17 @@
+class findpivotidx {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - nums[i] - leftSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+}
