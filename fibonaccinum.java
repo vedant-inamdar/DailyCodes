a4328ad8344bb785c9e7f040466540bf55ee453c
vedant-inamdar <vsinamdar8787@gmail.com>
2024-08-01 16:12:49 +0530

    done

--- fibonaccinum.java
+++ fibonaccinum.java
@@ -1,5 +1,6 @@
-class fibonaccinum {
-    public int fib(int n) {
+import java.util.*;
+public class fibonaccinum {
+    public static int fib(int n) {
         if(n==0){
             return 0;
         }else if(n==1){
@@ -7,5 +8,12 @@ class fibonaccinum {
         }else{
             return fib(n-1)+fib(n-2);
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int result=fib(n);
+        System.out.println(result);
+        sc.close();
     }
 }
